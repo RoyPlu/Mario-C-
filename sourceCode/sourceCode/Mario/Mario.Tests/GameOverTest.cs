af2a4efc259d18e737179be512348c63804eaca1
RoyPlu <roy.plu@hotmail.com>
2018-10-04 15:38:22 +0200

    Initial commit
    initial Super Mario Bros project

---
+++ sourceCode/sourceCode/Mario/Mario.Tests/GameOverTest.cs
@@ -0,0 +1,26 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mario.Tests
+{
+    [TestClass]
+    public class GameOverTest
+    {
+        [TestMethod]
+        public void GameOverTriggered()
+        {
+            var game = new GameObject("Mario");
+            ResourceManager.Instance.LoadSpriteSheetFromFile("sm-mario-sprites", @"resources\sm-mario-sprites.png", 10);
+            ResourceManager.Instance.LoadFontFromFile("arial", @"resources\arial.ttf");
+            var s = new MainScene(game) { Name = "play" };
+            game.SceneManager.AddScene(s);
+            var mario = new Characters.Mario(game);
+            for(var i = 0; i < 3; i++)
+            {
+                mario.Die();
+            }
+            var currentScene = game.SceneManager.CurrentScene.Name;
+            Assert.AreEqual(currentScene, "gameover");
+        }
+    }
+}
